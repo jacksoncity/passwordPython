7393e72b182c0e33678ce8fdd48ae7ae59177947
jacksoncity <jackson@DESKTOP-L6BECD1.localdomain>
2020-08-04 15:16:17 -0600

    added updated to display on password update button press.

--- Listener.py
+++ Listener.py
@@ -14,6 +14,7 @@ class listener:
         self.newRoot.resizable(False, False)
         self.newRoot.geometry("300x300+750+250")
         self.newRoot.title(name)
+        self.newRoot.configure(bg="grey")
 
         # check name to correspond to logo in top right
         if name == "YouTube":
@@ -25,6 +26,15 @@ class listener:
         elif name == "Amazon":
             self.newRoot.iconbitmap(
                 'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/amazonlogo.ico')
+        elif name == "Twitter":
+            self.newRoot.iconbitmap(
+                'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/twitterlogo.ico')
+        elif name == "Spotify":
+            self.newRoot.iconbitmap(
+                'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/spotifylogo.ico')
+        elif name == "Google":
+            self.newRoot.iconbitmap(
+                'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/googlelogo.ico')
         else:
             self.newRoot.iconbitmap(
                 'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/keyicon.ico')
@@ -34,6 +44,7 @@ class listener:
         self.passEntry.place(x=90, y=210)
         self.passLabel = tk.Label(self.newRoot, text="New Password: ")
         self.passLabel.place(x=110, y=185)
+        self.passLabel.configure(bg="grey")
         self.updateButton = tk.Button(self.newRoot, text="Update Password",
                                       command=lambda: listener.confirm(self, self.bounds),
                                       font=("Helvetica", 10, 'bold'),
@@ -41,10 +52,10 @@ class listener:
         self.updateButton.place(x=90, y=250)
         self.lines = open('passwords.txt', 'r').readlines()
         self.currentPass = self.lines[self.bounds]
-        self.textDisplay = tk.Label(self.newRoot, text = self.currentPass, font=("Helvetica", 25, 'bold'))
+        self.textDisplay = tk.Label(self.newRoot, text=self.currentPass, font=("Helvetica", 25, 'bold'), bg="grey")
         self.textDisplay.place(relx=.5, rely=.3, anchor="center")
 
-    # changes the password to what is inside of te textfield
+    # changes the password to what is inside of the textfield
     def confirm(self, num):
         password = self.passEntry.get()  # holds new password from the Entry widget
         line = num  # hte line to be written to (changes depending on button chosen)
@@ -52,8 +63,9 @@ class listener:
 
     def replace_line(self, fileName, lineNum, text):
         lines = open(fileName, 'r').readlines()
-        lines[lineNum] = text + '\n'  # adds '\n' so it keeps all other lines in tact, otherwise is slides whats
-                                      # in front of it to the line being edited
+        lines[lineNum] = text + '\n'  # adds '\n' so it keeps all other lines in tact, otherwise is slides what is
+                                      # below of it, to the line being edited
         out = open(fileName, 'w')
         out.writelines(lines)
+        self.textDisplay.configure(text=lines[lineNum]) # updates password on screen before closing txt file
         out.close()
--- Main.py
+++ Main.py
@@ -7,10 +7,11 @@ root = tk.Tk()
 root.resizable(False, False)
 root.geometry("450x450+680+130")
 root.title("Password Keeper")
+root.configure(bg="gray")
 root.iconbitmap(
     'c:/Users/jacks/Documents/DESKTOP/Personal_Programming/passwordPython/icons/keyicon.ico')
 
-# YouTube
+# paths for buttons placed on the main window
 youTube = tk.Button(root, text="YouTube", command=lambda: listener.updatePass(self, "YouTube", 0), font=("Helvetica", 15),
                     height=1, width=8)
 youTube.place(x=40, y=40)
@@ -22,6 +23,18 @@ reddit.place(x=180, y=40)
 amazon = tk.Button(root, text="Amazon", command=lambda: listener.updatePass(self, "Amazon", 2), font=("Helvetica", 15),
                    height=1, width=8)
 amazon.place(x=320, y=40)
+
+twitter = tk.Button(root, text="Twitter", command=lambda: listener.updatePass(self, "Twitter", 3), font=("Helvetica", 15),
+                   height=1, width=8)
+twitter.place(x=40, y=110)
+
+spotify = tk.Button(root, text="Spotify", command=lambda: listener.updatePass(self, "Spotify", 4), font=("Helvetica", 15),
+                   height=1, width=8)
+spotify.place(x=180, y=110)
+
+google = tk.Button(root, text="Google", command=lambda: listener.updatePass(self, "Google", 5), font=("Helvetica", 15),
+                   height=1, width=8)
+google.place(x=320, y=110)
 
 
 root.mainloop()
